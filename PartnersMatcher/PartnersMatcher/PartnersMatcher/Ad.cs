724d65b53f1264daea1cff38d69e1689dddba3fd
amitp <amitp@wix.com>
2017-01-01 20:39:24 +0200

    Created User, Ad and DatabaseUtils

---
+++ PartnersMatcher/PartnersMatcher/PartnersMatcher/Ad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartnersMatcher
+{
+    class Ad
+    {
+        private int _serialNumber;
+        private string _category;
+        private string _location;
+        private string _admin;
+
+        public Ad(int serialNumber, string category, string location, string admin)
+        {
+            _serialNumber = serialNumber;
+            _category = category;
+            _location = location;
+            _admin = admin;
+        }
+
+        public Ad() { }
+    }
+}
---
+++ PartnersMatcher/PartnersMatcher/PartnersMatcher/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartnersMatcher
+{
+    class User
+    {
+        private string _email;
+        private string _firstName;
+        private string _lastName;
+        private string _password;
+        private string _city;
+
+        public User(string email, string firstName, string lastName, string password, string city)
+        {
+            _email = email;
+            _firstName = firstName;
+            _lastName = lastName;
+            _password = password;
+            _city = city;
+        }
+
+        public User() { }
+    }
+}
