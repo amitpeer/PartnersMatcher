801029f10f0e15b092d3015c90fe5319554c1500
amitp <amitp@wix.com>
2017-01-24 18:47:25 +0200

    ikjh

--- PartnerMatcher/PartnersMatcher/Model/User.cs
+++ PartnerMatcher/PartnersMatcher/Model/User.cs
@@ -14,7 +14,6 @@ namespace PartnersMatcher.Model
         private string _password;
         private string _city;
         private List<int> _groups;
-        private User user;
 
         public User() { }
 
