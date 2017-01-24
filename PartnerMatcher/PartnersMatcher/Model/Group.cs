9ae7d7a33fd1f8490d8c18235e1afbc351865003
amitp <amitp@wix.com>
2017-01-24 15:48:37 +0200

    added id in group

--- PartnerMatcher/PartnersMatcher/Model/Group.cs
+++ PartnerMatcher/PartnersMatcher/Model/Group.cs
@@ -14,6 +14,7 @@ namespace PartnersMatcher.Model
         private List<Request> requests;
         private string content;
         private string title;
+        private int id;
 
         public Group()
         {
@@ -21,12 +22,13 @@ namespace PartnersMatcher.Model
             requests = new List<Request>();
         }
 
-        public Group(Ad ad, Admin admin, string content, string title)
+        public Group(Ad ad, Admin admin, string content, string title, int id)
         {
             this.ad = ad;
             this.admin = admin;
             this.content = content;
             this.title = title;
+            this.id = id;
             users = new List<User>();
             requests = new List<Request>();
         }
@@ -49,5 +51,7 @@ namespace PartnersMatcher.Model
         public List<Request> Request { get { return requests; } set { requests = value; } }
 
         public string Title { get { return title; } set { title = value; } }     
+
+        public int Id { get { return id; } set { id = value; } }
     }
 }
